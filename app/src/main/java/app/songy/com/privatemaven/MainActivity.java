e4cccab6571570c054e2d789cf37239e0a3386a1
songy <songy@aicaigroup.com>
2018-09-28 10:55:31 +0800

    gradle modify

--- app/src/main/java/app/songy/com/privatemaven/MainActivity.java
+++ app/src/main/java/app/songy/com/privatemaven/MainActivity.java
@@ -1,7 +1,12 @@
 package app.songy.com.privatemaven;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import app.songy.com.lib_rn.MyReactActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +14,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViewById(R.id.tv_go).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(MainActivity.this, MyReactActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
